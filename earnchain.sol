e8cc13cd9536182950fc6dd8aaa947145284e768
lucas-rosa95 <lucas.rosa95br@gmail.com>
2025-09-13 20:49:00 -0300

    defining the contract structs

--- earnchain.sol
+++ earnchain.sol
@@ -4,6 +4,20 @@ pragma solidity >=0.4.16 <0.9.0;
 contract Earnchain {
     address public owner;
 
+    enum BucketStatus { Open, Closed, Cancelled }
+
+    struct StructBucket {
+        address owner;
+        BucketStatus status;
+        uint256 entryAmount;
+        uint256 totalAmount;
+        uint limitParticipants;
+        uint subscriptionStartTime;
+        uint subscriptionEndTime;
+    }
+
+    mapping(uint => StructBucket) public buckets;
+
     constructor() {
         owner = msg.sender;
     }
